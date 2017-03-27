b05a265c914ceaefb9632f6192ea7efcddffc0be
KaneHui <18392888381@163.com>
2017-03-27 19:30:26 +0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -17,7 +17,7 @@ set +e
   echo "Use default Android CPU: armeabi-v7a"
   export FLOW_ANDROID_CPU_VERSION=armeabi-v7a
 
-array=$(find $FLOW_CURRENT_PROJECT_PATH -name *-androidTest-unaligned.apk 2>&1)
+array=$(find $FLOW_CURRENT_PROJECT_PATH -name *-unaligned.apk 2>&1)
 num=${#array[@]} 
 #如果num等于1,说明没有安装UI测试apk，接下来执行./gradlew assembleAndroidTest命令
 if [ $num -eq 1 ] ; then
