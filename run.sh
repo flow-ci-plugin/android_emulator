830432b556fb3466f06bd23657ff41e6500787a9
KaneHui <18392888381@163.com>
2017-03-27 19:30:58 +0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -36,7 +36,7 @@ emulator -avd test -no-audio -no-window -system /usr/sdk/android-sdk-linux/syste
 adb wait-for-device
 
 echo "等待120s,直到emulator完全启动"
-sleep 120
+sleep 110
 
 array=$(find $FLOW_CURRENT_PROJECT_PATH -name *-unaligned.apk 2>&1)
 for file in ${array[@]}
