1fc09b9f98b9deb553e07e8429f506378e648ef1
KaneHui <18392888381@163.com>
2017-03-28 10:30:57 +0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -19,32 +19,10 @@ set +e
 
 array=$(find $FLOW_CURRENT_PROJECT_PATH -name *-unaligned.apk 2>&1)
 num=${#array[@]} 
+echo $num
 #如果num小于等于1,说明没有安装UI测试apk，接下来执行./gradlew assembleAndroidTest命令
 if [ $num -le 1 ] ; then
-echo "can't find *-debug-androidTest.unaligned.apk"
+echo "can't find *-debug-androidTest-unaligned.apk"
 echo "./gradlew assembleAndroidTest"
 ./gradlew assembleAndroidTest
 fi
-
-cd /usr/sdk/android-sdk-linux/tools
-echo "android create avd -n test -t 22 --abi default/armeabi-v7a"
-echo no | android create avd -n test -t 22 --abi default/armeabi-v7a
-
-echo "emulator -avd test -no-audio -no-window -system /usr/sdk/android-sdk-linux/system-images/android-23/default/armeabi-v7a/system.img &
-adb wait-for-device"
-emulator -avd test -no-audio -no-window -system /usr/sdk/android-sdk-linux/system-images/android-23/default/armeabi-v7a/system.img &
-adb wait-for-device
-
-echo "等待105s,直到emulator完全启动"
-sleep 105
-
-array=$(find $FLOW_CURRENT_PROJECT_PATH -name *-unaligned.apk 2>&1)
-for file in ${array[@]}
-do
- echo "install APK: "
- adb install -r $file
-done
-
- instrumentation=$(adb shell pm list instrumentation|grep 'test/android'|awk -F ' ' '{print substr($1,17)}')
- echo "shell am instrument -w $instrumentation"
- adb shell am instrument -w $instrumentation
