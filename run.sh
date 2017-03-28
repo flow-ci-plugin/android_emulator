b5556fe132e33e0b7c1a2a10ae24f63fe88ca456
KaneHui <18392888381@163.com>
2017-03-28 09:47:14 +0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -20,7 +20,7 @@ set +e
 array=$(find $FLOW_CURRENT_PROJECT_PATH -name *-unaligned.apk 2>&1)
 num=${#array[@]} 
 #如果num等于1,说明没有安装UI测试apk，接下来执行./gradlew assembleAndroidTest命令
-if [ $num -eq 1 ] ; then
+if [ $num -le 1 ] ; then
 echo "can't find *-debug-androidTest.unaligned.apk"
 echo "./gradlew assembleAndroidTest"
 ./gradlew assembleAndroidTest
@@ -35,8 +35,8 @@ adb wait-for-device"
 emulator -avd test -no-audio -no-window -system /usr/sdk/android-sdk-linux/system-images/android-23/default/armeabi-v7a/system.img &
 adb wait-for-device
 
-echo "等待110s,直到emulator完全启动"
-sleep 110
+echo "等待105s,直到emulator完全启动"
+sleep 105
 
 array=$(find $FLOW_CURRENT_PROJECT_PATH -name *-unaligned.apk 2>&1)
 for file in ${array[@]}
