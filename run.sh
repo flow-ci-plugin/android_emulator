964dab9e72433d37c2a5b9694a2e76e1c5ce20f1
KaneHui <18392888381@163.com>
2017-03-28 10:44:41 +0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -17,12 +17,10 @@ set +e
   echo "Use default Android CPU: armeabi-v7a"
   export FLOW_ANDROID_CPU_VERSION=armeabi-v7a
 
-array=$(find $FLOW_CURRENT_PROJECT_PATH -name *-unaligned.apk 2>&1)
-num=${#array[@]} 
-echo $num
+array=$(find $FLOW_CURRENT_PROJECT_PATH -name *-androidTest-unaligned.apk 2>&1)
 #如果num小于等于1,说明没有安装UI测试apk，接下来执行./gradlew assembleAndroidTest命令
-if [ $num -le 1 ] ; then
+if [ -z "$array" ] ; then
 echo "can't find *-debug-androidTest-unaligned.apk"
 echo "./gradlew assembleAndroidTest"
-./gradlew assembleAndroidTest
+#./gradlew assembleAndroidTest
 fi
